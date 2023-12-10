a4115929718de4ce58f80787fe1cb7875779a768
GeraldJe <138845832+GeraldJe@users.noreply.github.com>
2023-12-10 18:38:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,32 +2,31 @@ const wrapper = document.querySelector(".wrapper");
 
 const question = document.querySelector(".question");
 
-const gif = document.querySelector(".gif")
+const gif = document.querySelector(".gif");
 
 const yesBtn = document.querySelector(".yes-btn");
 
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
+  question.innerHTML = "Aaaaa, I like you too";
 
-question.innerHTML = "I Love You too Aurelia Tiara Widana";
-
-gif.src = "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp"; });
+  gif.src =
+    "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
+});
 
 noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
 
-const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
 
-const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
 
-const maxY = window.innerHeight - noBtnRect.height;
+  const randomX = Math.floor(Math.random() * maxX);
 
-const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
 
-const randomY = Math.floor(Math.random() * maxY);
+  noBtn.style.left = randomX + "px";
 
-noBtn.style.left = randomX + "px";
-
-noBtn.style.top = randomY + "px";
-
+  noBtn.style.top = randomY + "px";
 });

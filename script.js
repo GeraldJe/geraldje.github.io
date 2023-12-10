6c950c7cb23f573dc50659d883e550fe7f60fc94
GeraldJe <138845832+GeraldJe@users.noreply.github.com>
2023-12-10 18:45:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Aaaaa, I like you too";
+  question.innerHTML = "Aaaaa, I love you too Aurelia Tiara Widana";
 
   gif.src =
     "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
